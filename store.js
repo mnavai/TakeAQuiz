e9ced02675a3cb94929f31e47d442675e8cc413a
mnavai <maryam.navaei@gmail.com>
2019-09-21 21:53:03 -0400

    Create store.js

---
+++ store.js
@@ -0,0 +1,45 @@
+const store = {
+
+  questions : [
+  {
+//1      
+  quesion : "What is the highest mount in the world?",
+  options : ["K2", "Lhotse", "Everest", "Makalu"],
+  answer: "Everest"
+  },
+//2
+  {
+    question: "What is the world’s wealthiest city?",
+    options : ["Hong Kong", "New York", "Paris", "Abu Dhabi"],
+    answer : "Hong Kong", 
+  },
+//3
+  {
+    question: "Where is world’s oldest city?",
+    options : ["Athens, Greece", "Plovdiv, Bulgaria", "Faiyum, Egypt", "Damascus, Syria"],
+    answer : "Damascus, Syria", 
+  },
+//4
+  {
+    question: "Which is the world’s most visited city?",
+    options : ["Dubai", "London", "Istanbul", "Tokyo"],
+    answer : "London", 
+  },
+//5
+  {
+    question: "Where is the world’s least populated city?",
+    options : ["Ngerulmud", "Funafuti", "Vatican City", "Fujairah"],
+    answer : "Ngerulmud", 
+  },
+//6
+  {
+    question: "Where is the driest spot on earth?",
+    options : ["Aoulef, Algeria", "Wadi Halfa, Sudan", "Atacama Desert, Chile", "Ica, Peru"],
+    answer : "Atacama Desert, Chile", 
+  } 
+  ],
+
+  currentQuestion: 0,
+  score: 0
+};   
+
