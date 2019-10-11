3209f3a4bc68628c99bbcff81b9e50e3ed601a35
Maryam <maryam.navaei@gmail.com>
2019-10-10 21:39:36 -0400

    Update store.js

--- store.js
+++ store.js
@@ -1,45 +1,38 @@
-const store = {
-
-  questions : [
-  {
-//1      
-  quesion : "What is the highest mount in the world?",
-  options : ["K2", "Lhotse", "Everest", "Makalu"],
-  answer: "Everest"
+const store = [ 
+//1  
+  {    
+  question : "What is the highest mount in the world?",
+  answers : ["K2", "Lhotse", "Everest", "Makalu"],
+  correctAnswer: "Everest"
   },
 //2
   {
     question: "What is the world’s wealthiest city?",
-    options : ["Hong Kong", "New York", "Paris", "Abu Dhabi"],
-    answer : "Hong Kong", 
+    answers : ["Hong Kong", "New York", "Paris", "Abu Dhabi"],
+    correctAnswer : "Hong Kong" 
   },
 //3
   {
     question: "Where is world’s oldest city?",
-    options : ["Athens, Greece", "Plovdiv, Bulgaria", "Faiyum, Egypt", "Damascus, Syria"],
-    answer : "Damascus, Syria", 
+    answers : ["Athens, Greece", "Plovdiv, Bulgaria", "Faiyum, Egypt", "Damascus, Syria"],
+    correctAnswer : "Damascus, Syria" 
   },
 //4
   {
     question: "Which is the world’s most visited city?",
-    options : ["Dubai", "London", "Istanbul", "Tokyo"],
-    answer : "London", 
+    answers : ["Dubai", "London", "Istanbul", "Tokyo"],
+    correctAnswer : "London" 
   },
 //5
   {
     question: "Where is the world’s least populated city?",
-    options : ["Ngerulmud", "Funafuti", "Vatican City", "Fujairah"],
-    answer : "Ngerulmud", 
+    answers : ["Ngerulmud", "Funafuti", "Vatican City", "Fujairah"],
+    correctAnswer : "Ngerulmud"
   },
 //6
   {
     question: "Where is the driest spot on earth?",
-    options : ["Aoulef, Algeria", "Wadi Halfa, Sudan", "Atacama Desert, Chile", "Ica, Peru"],
-    answer : "Atacama Desert, Chile", 
+    answers : ["Aoulef, Algeria", "Wadi Halfa, Sudan", "Atacama Desert, Chile", "Ica, Peru"],
+    correctAnswer : "Atacama Desert, Chile" 
   } 
-  ],
-
-  currentQuestion: 0,
-  score: 0
-};   
-
+];
